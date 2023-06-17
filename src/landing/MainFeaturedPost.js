e283d4c979c590e0b3d6a73544644d460420eea0
kelocoes <kevin.rodriguez109@gmail.com>
2023-06-17 17:47:44 -0500

    Fix codesmells

--- src/landing/MainFeaturedPost.js
+++ src/landing/MainFeaturedPost.js
@@ -1,5 +1,4 @@
 import * as React from 'react'
-// import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
